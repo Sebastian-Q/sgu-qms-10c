8188b667e822f96f1f97415ffe812eade7b732f4
Sebastian-Q <20203tn049@utez.edu.mx>
2025-11-16 16:17:39 -0600

    [UPDATE] Modificaciones para aplicar HTTPS

--- client/src/models/user/user.controller.js
+++ client/src/models/user/user.controller.js
@@ -1,7 +1,7 @@
 const UserController = {};
 const ENV = import.meta.env;
 
-const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
+const API_URL = `${ENV.VITE_API_PROTOCOL}://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
 
 UserController.getAll = async () => {
     try {
